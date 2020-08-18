359243ad2b59925480c4f419f0171e2499a8efd3
arki-v1 <arrakirk@gmail.com>
2020-08-18 12:22:42 +0100

    started work on the download function and added usages in the help box

--- dotnet-src/Program.cs
+++ dotnet-src/Program.cs
@@ -10,18 +10,22 @@ namespace Pigeon
         {
             string gateway;
             int port;
+            string faddress;
             string[] settings;
             if(args.Length == 0)
             {
-                Console.WriteLine("NO ARGUMENTS GIVEN - USE -h FOR HELP");
+                Console.WriteLine("NO ARGUMENTS GIVEN - USE help FOR HELP");
             }
-            else if(args [0] == "help")
+            else if(args[0] == "help")
             {
                 Console.WriteLine("PIGEON FILE TRANSFER VERSION 0");
                 Console.WriteLine("------------------------------");
                 Console.WriteLine("help =  show help box");
+                Console.WriteLine("usage: pigeon help");
                 Console.WriteLine("send = sends a file from your computer to another ip address");
+                Console.WriteLine("usage: pigeon send [FILE ADDRESS] [PORT (overriding settings file)]");
                 Console.WriteLine("get = gets a file from remote ip address");
+                Console.WriteLine("usage: pigeon get [IP ADDRESS] [PORT] [NEW FILE ADDRESS]");
                 Console.WriteLine("------------------------------");
             }
             else if(args[0] == "send")
@@ -47,7 +51,17 @@ namespace Pigeon
             }
             else if(args[0] == "get")
             {
-
+                if(args.Length == 4)
+                {
+                    gateway = args[1];
+                    port = int.Parse(args[2]);
+                    faddress = args[3];
+                }
+                else
+                {
+                    Console.WriteLine("Argument Length not correct. Exiting.");
+                    return;
+                }
             }
         }
     }
